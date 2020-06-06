5bbcd7490cab45001703eb3c153d02979d8ebc1a
mmmhobin2@naver.com <mmmhobin2>
2020-06-06 23:26:04 +0900

    변수선언

---
+++ 변수선언.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	
+	// 변수를 선언. 
+	
+	//int 123123123123123 _a;// 변수선언할때 첫번째에 숫자 금지.
+	
+	int age=205;
+	char letter_grade='A';
+	float weight=4.1;
+	
+	
+	printf("%d\n",age);
+	printf("%c\t\n",letter_grade);
+	printf("%.2f",weight); 
+	 
+	
+	return 0;
+}
