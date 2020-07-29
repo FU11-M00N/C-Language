9b969b557fe7c907196934d14f95f51f388de566
mmmhobin2@naver.com <mmmhobin2>
2020-07-29 18:50:57 +0900

    포인터함수(기초)

---
+++ 포인터함수.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+//Prosto
+
+void ChangePlusOne(int n) {
+
+	n += 1;
+
+}
+
+int main(void) {
+
+	int number = 3;
+
+	printf("%d\n", number);
+
+
+
+	number = 5;
+
+	printf("%d\n", number);
+
+
+
+	ChangePlusOne(number);
+
+	printf("%d\n", number);
+
+
+
+	return 0;
+
+}
+
+
+
